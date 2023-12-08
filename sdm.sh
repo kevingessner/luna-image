116458209ae7f52826a6f7e1286c6bfcf432c20e
Kevin Gessner <kevin@kevingessner.com>
2023-12-08 11:42:38 -0500

    fixes and hotspot

--- sdm.sh
+++ sdm.sh
@@ -6,6 +6,12 @@ OUT_FINAL=${1-"$DIR/luna.img"}
 RASPIOS_IMAGE_URL=https://downloads.raspberrypi.org/raspios_oldstable_lite_armhf/images/raspios_oldstable_lite_armhf-2023-12-06/2023-12-05-raspios-bullseye-armhf-lite.img.xz
 RASPIOS_IMAGE_FILE=$(basename "$RASPIOS_IMAGE_URL")
 
+LUNA_USERNAME=luna
+LUNA_UID=1001
+LUNA_SDM_COPYDIR="copydir:from=$DIR/../luna|to=/home/$LUNA_USERNAME/|rsyncopts=--archive --cvs-exclude --info=progress2 --chown=$LUNA_UID:$LUNA_UID"
+
+
+
 if ! [[ -f "$OUT_BASE" ]]
 then
     if ! [[ -f "$RASPIOS_IMAGE_FILE" ]]
@@ -17,26 +23,27 @@ then
 
     sudo sdm \
         --customize "$OUT_BASE" \
-        \ # required on WSL \
-        --chroot \
+        --chroot `# required on WSL` \
         --poptions noupdate,noupgrade,noautoremove \
         --extend --xmb 18000 \
-        --plugin user:"adduser=luna" \
-        --plugin $DIR/authorized_keys.sh:"user=luna|keyfile=$HOME/.ssh/rpi_luna_ssh_20230511.pub" \
-        --plugin copydir:"from=$DIR/../luna|to=/home/luna/|rsyncopts=--archive --cvs-exclude --info=progress2"
+        --plugin user:"adduser=$LUNA_USERNAME|uid=$LUNA_UID" \
+        --plugin $DIR/authorized_keys.sh:"user=$LUNA_USERNAME|keyfile=$HOME/.ssh/rpi_luna_ssh_20230511.pub" \
+        --plugin "$LUNA_SDM_COPYDIR"
 fi
 
-cp "$OUT_BASE" "$OUT_FINAL"
+rsync --progress --update "$OUT_BASE" "$OUT_FINAL"
 
 sudo sdm \
     --customize "$OUT_FINAL" \
     --redo-customize \
-    \ # required on WSL \
-    --chroot \
+    --chroot `# required on WSL` \
     --poptions noupdate,noupgrade,noautoremove \
-    --plugin apps:"apps=autoconf,imagemagick" \
+    --plugin apps:"apps=autoconf,imagemagick,python3-venv,fontconfig,fonts-liberation,fonts-urw-base35" \
     --plugin disables:"piwiz|triggerhappy" \
     --plugin L10n:"keymap=us|locale=en_US.UTF-8|timezone=UTC" \
     --plugin network:"wifissid=TP-Link_C930|wifipassword=09744165|wificountry=US" \
+    --plugin hotspot:"domain=luna.local|type=local|ssid=luna-setup|passphrase=luna-setup|hwmode=g|channel=3" \
+    `# update the luna files in the base image; should be quick if the PNGs are unchanged` \
+    --plugin "$LUNA_SDM_COPYDIR" \
     --hostname mypi1 \
     --regen-ssh-host-keys
